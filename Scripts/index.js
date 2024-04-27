df81a4d7ce238da5f0a31f9ce2b402e23519d5f9
ivyycc <ivy.chepkwony@gmail.com>
2024-04-27 12:09:43 +0200

    fixing indexes

---
+++ Scripts/index.js
@@ -0,0 +1,34 @@
+const menu = [
+    {name: "Homepage", href: "Index.html"},
+    {name: "Blogs", href: "Blogs/index.html"},
+    {name: "Designs", href: "Designs/index.html"},
+    {name: "Portfolio", href: "Portfolio/Porfolio.html"},
+    {name: "Profile", href: "Profile/Profile.html"},
+
+]
+
+
+
+
+
+
+//function createMenu(){
+  //  const nav = document.querySelector("nav");//fetches nav from doc 
+   // const ul = document.createElement("ul");
+
+  //  for(let item of ['index', 'Blogs', 'Designs', 'Essays']){
+
+    //    const li = document.createElement("li");
+    //    li.innerText = item.text;
+
+       // ul.appendChild(li);
+
+
+  //  }
+
+ //   nav.appendChild(ul);
+
+
+//};
+
+//document.addEventListener("DOMContentLoaded",()=> createDiffieHellmanGroup())
