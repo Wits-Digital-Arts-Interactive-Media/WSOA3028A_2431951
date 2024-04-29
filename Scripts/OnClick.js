f96b23ede9fcd358ea58738287ba6d48009d249e
ivyycc <ivy.chepkwony@gmail.com>
2024-04-29 03:43:26 +0200

    fixing errors

--- Scripts/OnClick.js
+++ Scripts/OnClick.js
@@ -1,36 +1,36 @@
 
-const imageIds = [{pos: 1, name:"image1", clicked:false}, {pos: 2,name:"image2", clicked:false}, {pos: 3,name:"image3", clicked:false},];
-const textIds = [{pos: 1,name:"text1"}, {pos: 2,name:"text2"}, {pos: 3,name:"text3"},];
+
 
 
 function click()
 {
-    const images = document.querySelectorAll("img");
-    const texts = document.querySelectorAll("div");
 
-    images.forEach(image => {image.addEventListener("click", convert())})
-
-    //var image = document.getElementById(item.name);
-   // image.addEventListener("click", convert(item.pos));
-
- 
+    var image = document.getElementById("image1");
+    
+    image.addEventListener("click", convert());
+    
 };
-
-function textUnhidden(text)
-{
-    text.hidden = false;
-}
 
 
 function convert() {
 
-
-    const texts = document.querySelectorAll("div");
-
-    texts.forEach(text => {textUnhidden(text);})
-    
-    console.log("Clicked!");
+    let text = document.getElementById("text1"); 
+    console.log("done");
+    text.hidden = false;
    
 };
 
-document.addEventListener("DOMContentLoaded",()=> click())
+document.addEventListener("DOMContentLoaded",()=> click());
+
+/*
+const imageIds = [{ name:"image1"}, {name:"image2"}, {name:"image3"},];
+const textIds = [{name:"text1"}, {name:"text2"}, {name:"text3"},];
+   const images = document.querySelectorAll("img");
+    const texts = document.querySelectorAll("div");
+
+    images.forEach(image => {image.addEventListener("click", convert())})
+      /*const texts = document.querySelectorAll("div");
+
+    texts.forEach(text => {textUnhidden(text);})
+    
+    console.log("Clicked!");*/
