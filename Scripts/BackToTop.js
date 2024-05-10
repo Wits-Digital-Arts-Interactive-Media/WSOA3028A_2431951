5bbe2f2e3ecaecc3689b091d54155ba835e8fef1
ivyycc <ivy.chepkwony@gmail.com>
2024-05-10 12:16:41 +0200

    editing

--- Scripts/BackToTop.js
+++ Scripts/BackToTop.js
@@ -1,25 +1,2 @@
-let mybutton = document.getElementById("scrollUp");
-
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    console.log("display");
-    mybutton.display = true;
-  } else {
-    console.log("not displayed");
-    mybutton.display = false;
-  }
-}
-
-
-function topFunction() {
-
-  console.log("clicked");
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-export function click()
-{
-    window.onscroll = function() {scrollFunction()};
-    mybutton.onclick = topFunction();
-}
+//Back To Top Function 
+function scrollUp(){window.scrollTo({top:0, behavior: "smooth"});}
